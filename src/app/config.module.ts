64d2a2cb6ca3f5526f7dc2b36b8bddfdf245246e
José Rafael Morente González <josemorenteg98@outlook.com>
2018-04-25 23:26:02 -0600

    [FIX]
    Update link.

--- src/app/config.module.ts
+++ src/app/config.module.ts
@@ -1,2 +1,2 @@
-//export const path:any = { path:"http://jbackend.herokuapp.com" }
-export const path:any = { path:"http://localhost:3000" }
+export const path:any = { path:"http://jbackend.herokuapp.com" }
+//export const path:any = { path:"http://localhost:3000" }
